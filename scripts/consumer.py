03cbd645fd11d8d09024ac7fdfa1fb13675ca71b
Tatian05 <isp2014asje@gmail.com>
2025-03-28 13:47:23 -0300

    docker postgres database added

--- scripts/consumer.py
+++ scripts/consumer.py
@@ -82,11 +82,11 @@ kafka_query = df_kafka \
 #POSTGRESQL CONNECTION
 load_dotenv()
 
-db_name = os.environ.get("DB_NAME")
-db_host = os.environ.get("DB_HOST")
-db_port = os.environ.get("DB_PORT")
-db_user = os.environ.get("DB_USER")
-db_password = os.environ.get("DB_PASSWORD")
+db_name = os.environ.get("POSTGRES_DB")
+db_host = os.environ.get("POSTGRES_HOST")
+db_port = os.environ.get("POSTGRES_PORT")
+db_user = os.environ.get("POSTGRES_USER")
+db_password = os.environ.get("POSTGRES_PASSWORD")
 
 postgres_url = f"jdbc:postgresql://{db_host}:{db_port}/{db_name}"
 db_properties = {
